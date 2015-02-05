58de4ddb5fa22936114e52fc07ecfaf7d2641f44
Frank Eslami <eslamif@oregonstate.edu>
2015-02-04 22:44:00 -0800

    loopback object structure implemented

--- src/loopback.php
+++ src/loopback.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "R1 <br>";
+
 
 
 //Determine server request type
@@ -12,14 +12,35 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 else if($_SERVER['REQUEST_METHOD'] == 'GET') {
 	echo "It's a GET request! <br>";
-
-	foreach($_GET as $key => $value) {
-		echo "$key : $value <br>";
-	}
+	httpToJson($_GET);
 
 }
 
 
 
-//echo htmlspecialchars($_GET["name"]);
+//Convert httpRequest to JSON object
+function httpToJson($type) {
+	foreach($type as $key => $value) {
+		echo "$key : $value <br>";	
+	}
+	
+	//Structure array to {"Type":"[GET|POST]","parameters":{"key1":"value1", ... ,"keyn":"valuen"}}
+	$httpType =  $_SERVER['REQUEST_METHOD'];
+	$type = array('parameters'=>$type);				//add second array for parameters
+	$type = array('Type'=>$httpType) + $type;		//prepend type:GET|POST
+	
+
+	
+	
+
+	
+	
+	global $jsonObject;
+	$jsonObject = json_encode($type);
+	echo $jsonObject;
+}
+
+
+
+
 ?>
