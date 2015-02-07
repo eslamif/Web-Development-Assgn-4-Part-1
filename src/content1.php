f5ac08c68bb6d2140b0ec46d3fb4fbb9dc1770ce
Frank Eslami <eslamif@oregonstate.edu>
2015-02-07 11:39:48 -0800

    login - created form & tested submission:

---
+++ src/content1.php
@@ -0,0 +1,8 @@
+<?php
+//Enable error detection
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "It worked";
+
+?>
---
+++ src/login.php
@@ -0,0 +1,18 @@
+<?php
+//Enable error detection
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//***************************************************************************************************
+//***************************************************************************************************
+//CHANGES TO MAKE BEFORE SUBMITTING
+//form action to ONID path
+?>
+
+<!-- Username form -->
+<form action="http://localhost/myhost-exemple/cs290-ass4-p1/src/content1.php" method="POST">
+	<input type="text" name="username" > Username
+	<br>
+	<button type="submit">Login</button>
+	<!--<button name="login">Login</button>-->
+</form>
