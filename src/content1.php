81faf9cc4df2a054554929ebe78f15ce976d37ae
Frank Eslami <eslamif@oregonstate.edu>
2015-02-08 11:56:19 -0800

    content1.php - login, username validation, session init successful. Implement logout next

--- src/content1.php
+++ src/content1.php
@@ -2,7 +2,70 @@
 //Enable error detection
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+header('Content-Type: text/html');
 
-echo "It worked";
+$username = $_POST['username'];
+
+$loggedIn = validateUsername($username);
+$session = session($_POST);
+greetUser($session);
+
+
+/*------ FUNCTION DEFINITIONS ------*/
+//Validate username
+function validateUsername($username) {
+	//Username entered?
+	if($username == "" || $username == NULL) {
+		echo "A username must be entered. Click 
+		<a href=http://localhost/myhost-exemple/cs290-ass4-p1/src/login.php>here</a> to return to the Login screen.";
+		return false;
+	}	
+	else
+		return true;
+}
+
+//Track Session
+function session($http) {
+	//Start and End session
+	session_start();
+	if(isset($http['action']) && $http['action'] == 'end') {
+		//End session
+		$_SESSION = array();
+		session_destroy();
+		
+		//Redirect user
+		$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+		$filePath = implode('/', $filePath);
+		$redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
+		header("Location: {$redirect}/Logout.html", true);
+		die();		
+	}
+	
+	//Active session - write and read
+	if(session_status() == PHP_SESSION_ACTIVE) {
+		if(isset($http['username'])) {
+			$_SESSION['username'] = $http['username'];
+		}
+		
+		if(isset($_SESSION['visits'])) {
+			$_SESSION['visits'] = 0;
+		}
+		
+		$_SESSION['visits']++;
+	}
+	return $_SESSION;
+}
+
+//Greet user
+function greetUser($session) {
+	$_SESSION['visits']--;
+	echo "Hi $_SESSION[username], you have visited this page $_SESSION[visits] times. \n";
+	echo "Click <a href=http://localhost/myhost-exemple/cs290-ass4-p1/src/login.php?logout=true>here</a> to logout";
+}
+
+
+
+
+
 
 ?>
--- src/login.php
+++ src/login.php
@@ -7,7 +7,14 @@ ini_set('display_errors', 1);
 //***************************************************************************************************
 //CHANGES TO MAKE BEFORE SUBMITTING
 //form action to ONID path
+
+
+//Log out of SESSION?
+if(isset($_GET['logout']) && $_GET['logout'] == true) {
+	echo "Log out";
+}
 ?>
+
 
 <!-- Username form -->
 <form action="http://localhost/myhost-exemple/cs290-ass4-p1/src/content1.php" method="POST">
