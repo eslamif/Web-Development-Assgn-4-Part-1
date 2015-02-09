801a4aa54f38093b607fc8657c052123fda66c19
Frank Eslami <eslamif@oregonstate.edu>
2015-02-08 17:46:17 -0800

    Tracking works. Next is end session

--- src/content1.php
+++ src/content1.php
@@ -4,26 +4,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-Type: text/html');
 
-$username = $_POST['username'];
-
-$loggedIn = validateUsername($username);
-$session = session($_POST);
-greetUser($session);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	session($_POST);
+}
+else if($_SERVER['REQUEST_METHOD'] == 'GET') {
+	session($_GET);	
+}
 
 
 /*------ FUNCTION DEFINITIONS ------*/
-//Validate username
-function validateUsername($username) {
-	//Username entered?
-	if($username == "" || $username == NULL) {
-		echo "A username must be entered. Click 
-		<a href=http://localhost/myhost-exemple/cs290-ass4-p1/src/login.php>here</a> to return to the Login screen.";
-		return false;
-	}	
-	else
-		return true;
-}
-
 //Track Session
 function session($http) {
 	//Start and End session
@@ -34,33 +23,53 @@ function session($http) {
 		session_destroy();
 		
 		//Redirect user
+		/*
 		$filePath = explode('/', $_SERVER['PHP_SELF'], -1);
 		$filePath = implode('/', $filePath);
 		$redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
 		header("Location: {$redirect}/Logout.html", true);
-		die();		
+		die();	
+		*/
 	}
 	
 	//Active session - write and read
 	if(session_status() == PHP_SESSION_ACTIVE) {
+		validateUsername($_POST['username']);
+		
+		//Validate username
 		if(isset($http['username'])) {
 			$_SESSION['username'] = $http['username'];
 		}
 		
-		if(isset($_SESSION['visits'])) {
+		//Set user status as logged in
+		if(!isset($_SESSION['loggedIn'])) {
+			$_SESSION['loggedIn'] = true;
+		}
+		
+		//Track number of visits
+		if(!isset($_SESSION['visits'])) {
 			$_SESSION['visits'] = 0;
 		}
 		
 		$_SESSION['visits']++;
+		
+		greetUser();
 	}
-	return $_SESSION;
+}
+
+//Validate username
+function validateUsername($username) {
+	//Username entered?
+	if($username == "" || $username == NULL) {
+		echo "A username must be entered. Click 
+		<a href=http://localhost/myhost-exemple/cs290-ass4-p1/src/login.php>here</a> to return to the Login screen.";
+	}
 }
 
 //Greet user
-function greetUser($session) {
-	$_SESSION['visits']--;
+function greetUser() {
 	echo "Hi $_SESSION[username], you have visited this page $_SESSION[visits] times. \n";
-	echo "Click <a href=http://localhost/myhost-exemple/cs290-ass4-p1/src/login.php?logout=true>here</a> to logout";
+	echo "Click <a href=http://localhost/myhost-exemple/cs290-ass4-p1/src/login.php>here</a> to logout";
 }
 
 
